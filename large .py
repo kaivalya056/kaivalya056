e6dd389e05c80c30be545b8691746435978f292d
kaivalya056 <112381426+kaivalya056@users.noreply.github.com>
2022-09-27 14:39:25 +0530

    Add files via upload

---
+++ large .py
@@ -0,0 +1,13 @@
+print("Enter the three numbers")
+print('a=')
+a=int(input())
+print('b=')
+b=int(input())
+print('c=')
+c=int(input())
+if a>b and a>c:
+      print("a is greatest number")
+elif b>a and b>c:
+      print("b is greatest number")
+else:
+      print("c is greatest number")
