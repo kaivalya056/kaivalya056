3b3824c0f1d6a90b7f3649efe75e5eb172576ec2
kaivalya056 <112381426+kaivalya056@users.noreply.github.com>
2022-09-28 10:04:55 +0530

    Add files via upload

---
+++ max number.py
@@ -0,0 +1,16 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/kolla/AppData/Local/Programs/Python/Python310/max number.py
+Enter the number of numbers 5
+Enter the Number: 1
+Enter the Number: 4
+Enter the Number: 3
+Enter the Number: 9
+Enter the Number: 6
+M= 2
+N= 1
+2 max number is 6
+1 min number is 1
+sum=  7
+Difference=  5
